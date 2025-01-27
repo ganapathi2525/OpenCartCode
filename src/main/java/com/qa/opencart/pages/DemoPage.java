b7a6efef101a54bd3e93d71a91d67890a60a8bef
unknown <pichika.ganapathi@Mphasis.com>
2025-01-27 11:12:47 +0530

    modified demo page

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -1,7 +1,18 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class DemoPage {
 	
 	int i=0;
+	private By name=By.id("name");
+	
+
+	public void nameIs() {
+		System.out.println("Demo Name");
+	}
+	
+	
+
 
 }
